579b6aa756ee8ea54da0d433edf8f90c8e7e15fd
christian <christiancasey969@gmail.com>
2017-12-27 18:00:16 +0000

    toggle menu slide effect - completed with background image

--- online-tutorials/toggle-icon-horizontal-menu-slide-effect/main.js
+++ online-tutorials/toggle-icon-horizontal-menu-slide-effect/main.js
@@ -5,5 +5,6 @@ console.log('JS Loaded!');
 $(document).ready(function() {
   $('.menu-toggle').click(function(){
     $('.menu-toggle').toggleClass('active');
+    $('nav').toggleClass('active');
   });
 });
